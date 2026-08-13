2119defb183387dc331fb99221f1aa488caa5442
Christopher Laino <131606156+ChristopherLaino@users.noreply.github.com>
2026-08-13 13:09:09 +0200

    primo commit

---
+++ Assets/Scenes/Test B/Test_B_code.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test_B_code : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Fatto Cacca Scarsa");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Test A/Test_A_code.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test_A_code : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Fatto Cacca Potente");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
