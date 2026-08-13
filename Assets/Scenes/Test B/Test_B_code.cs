8f319a853c840f281b2d74ab921b94f5c7a57810
Christopher Laino <131606156+ChristopherLaino@users.noreply.github.com>
2026-08-13 16:48:49 +0200

    cacca 2

--- Assets/Scenes/Test B/Test_B_code.cs
+++ Assets/Scenes/Test B/Test_B_code.cs
@@ -5,7 +5,7 @@ public class Test_B_code : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Fatto Cacca molto Scarsa");
+        print("Fatto Cacca Minna tosta Scarsa");
     }
 
     // Update is called once per frame
